3c99bc28490bec7fb66fa4a214935f43e1283adb
Cloe Kouadjo <armelfli50@gmail.com>
2019-10-14 14:33:22 +0000

    Automatically backed up by Learn

--- db/migrate/20191014143133_create_posts.rb
+++ db/migrate/20191014143133_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.text :description
+      t.timestamps null: false
     end
   end
 end
